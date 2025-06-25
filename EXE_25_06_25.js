37681bec6fdade257b3afdcc22e81e98c86b52b5
Oryan Menachem <om3160204@gmail.com>
2025-06-25 12:35:41 +0300

    EXE

--- EXE_25_06_25.js
+++ EXE_25_06_25.js
@@ -4,7 +4,10 @@ function Phone(brand, model)
 {
     this.brand = brand;
     this.model = model;
-    this.details = function() {console.log(`brand: ${brand} model: ${model}`);
+    this.details = function() {console.log(`brand: ${brand}. model: ${model}.`)
     }
 
 }
+
+const P1 = new Phone("Samsung", "S22");
+P1.details()
