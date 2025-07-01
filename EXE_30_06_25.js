0db9409b17db29f2330c1675b6d924bba6322849
Oryan Menachem <om3160204@gmail.com>
2025-07-01 14:53:27 +0300

    EXE

--- EXE_30_06_25.js
+++ EXE_30_06_25.js
@@ -1,22 +1,46 @@
+import fs, { readdir, readFile, stat } from 'fs';
+// import path from 'path';
+
 //  exercise 1
 
-import fs from 'fs';
+
+const f1Path = '../30_06_2025/f1';
+
+const p1 = new Promise((resolve, reject) => {
+
+    readFile('../30_06_2025/f1','utf-8', (err, data) => {
+
+    if (err) {reject(err); return;}
+
+    resolve(data)
+    })
 
 
-fs.readFile('./file.txt', 'utf-8', (err, data) => { 
-    
-    if (err) {console.log('error: ', err); return;}
-    console.log(data);
-    console.log(`length:`, data.length);
-})
+});
 
-fs.stat('./file.txt', (err, data) => { 
-    
-    if (err) {console.error('error: ', err); return;}
-    console.log(`birthtime:`, data.birthtime.toDateString());
-    console.log(`byte:` ,data.size);
-    
-    
-})
 
+p1.then((content, f1Path) => {
+    
+    console.log(`content:`, content),
+
+    stat('../30_06_2025/f1', (err, stats) => {
+
+        if (err) {console.log(err); return;}
+
+        console.log(`size:`, stats.size);
+        console.log(`birthtime:`,stats.birthtime);
+
+    })}
+
+
+);
+        
+        
+
+
+
+
+
+
+//  exercise 2
 
