102d295e98d74f56b56eb4a7ea28dc7d0dfeeef5
Oryan Menachem <om3160204@gmail.com>
2025-07-02 12:19:43 +0300

    EXE

--- EXE_02_07_25.js
+++ EXE_02_07_25.js
@@ -1,1 +1,33 @@
 // exercise 1
+
+// function getUser(userId) {
+
+//     fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+
+//     .then(resolve => resolve.json() )
+
+//     .then(date => console.log(date.name) )
+
+//     .catch(err => console.log(err))
+// }
+
+
+// getUser(2);
+
+
+
+// exercise 2
+
+function getPost(postId) {
+     
+     fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+
+    .then(resolve => resolve.json() )
+
+    .then(date => console.log(`Post title:  ${date.title}`) )
+
+    .catch(err => console.log(err))
+
+}
+
+getPost(3);
