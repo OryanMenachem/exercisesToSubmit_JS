7fd5161debfd63dc8ea0fbf38701c2a03d4b7051
Oryan Menachem <om3160204@gmail.com>
2025-07-02 16:13:34 +0300

    EXE

---
+++ EXE_asyncAwait_02_07_25.js
@@ -0,0 +1,39 @@
+import { error } from 'node:console';
+import * as fsp from 'node:fs/promises';
+
+// EXERCISE 1
+
+// async function readFilePromise(path) {
+
+//     try {
+
+//         const data = await fsp.readFile(path, 'utf-8')
+
+//         if (!data) { throw new Error('error'); }
+
+//         return data;
+//     }
+
+//     catch (error) { return error.message }
+
+
+// }
+
+
+// const p1 = await readFilePromise('./file.txt')
+
+// console.log(p1);
+
+
+
+
+// EXERCISE 2
+
+
+
+
+
+
+
+// EXERCISE 3
+// EXERCISE 4
